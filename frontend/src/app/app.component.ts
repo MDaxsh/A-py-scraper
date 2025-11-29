dad040a416024f905b17e3d1c4e6cf333337c93b
Mdaxsh <daxshd54@gmail.com>
2025-11-29 12:58:23 -0600

    fix run

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,28 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'My Application';
   backendStatus = 'Loading...';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
     this.checkBackendHealth();
   }
 
   checkBackendHealth() {
     this.http.get<any>('http://localhost:5000/api/health')
-      .subscribe(
-        (response) => {
+      .subscribe({
+        next: (response: any) => {
           this.backendStatus = response.status;
         },
-        (error) => {
+        error: (error: any) => {
           this.backendStatus = 'Connection error';
         }
-      );
+      });
   }
 }
